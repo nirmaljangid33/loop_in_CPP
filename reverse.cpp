774581a28a1c7b823a06ea5b4ca4463e9473ea7f
nirmal jangid <jangidnirmal15@gmail.com>
2025-07-22 21:25:58 +0530

    reverse number

---
+++ reverse.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i=1,n =1234;
+    cout<<"Enter the number = ";
+    cin>>n;
+    while(n>=i){
+        cout<<n<<endl;
+        n--;
+    }
+    return 0;
+
+}
